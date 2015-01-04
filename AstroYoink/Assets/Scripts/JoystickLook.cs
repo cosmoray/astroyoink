77875b1edc0f932b11a4615151ba07e7c71e491c
Cosmo Ray <Cosmo@cosmo-rays-imac.gateway.sonic.net>
2015-01-04 14:47:23 -0800

    Added CameraWithForceLook prefab

---
+++ AstroYoink/Assets/Scripts/JoystickLook.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickLook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+		rigidbody.AddTorque (Input.GetAxis ("Vertical"), Input.GetAxis ("Horizontal"), 0);
+	}
+}
