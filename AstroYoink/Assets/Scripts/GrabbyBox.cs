26bdbf643d13d012fa04d3e1484d7fddfcb9360e
nathanhinchey <devnph@gmail.com>
2015-01-04 17:08:03 -0800

    Added GrabbyBox prefab and script

---
+++ AstroYoink/Assets/Scripts/GrabbyBox.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabbyBox : MonoBehaviour {
+
+	GameObject grabbableThing;
+
+	void OnTriggerEnter(Collider other){
+		if (grabbableThing.tag == "Grabbable")
+			grabbableThing = other.gameObject;
+	}
+	void OnTriggerExit(Collider other){
+		grabbableThing = null;
+	}
+}
