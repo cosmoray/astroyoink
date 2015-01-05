5feea4bbeb7c442d54d43bb5ab23b51ebbd2c7c2
Cosmo Ray <Cosmo@cosmo-rays-imac.gateway.sonic.net>
2015-01-04 16:42:25 -0800

    ColorObject and GrabObject Scripts

---
+++ AstroYoink/Assets/Scripts/GrabObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabObject : MonoBehaviour {
+
+	public float SpringIntensity = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.G))
+		{
+			gameObject.GetComponent<SpringJoint>().spring =  SpringIntensity;
+		
+	}
+		if(Input.GetKeyUp(KeyCode.G))
+		{
+			gameObject.GetComponent<SpringJoint>().spring =  0f;
+		}
+	}
+}
