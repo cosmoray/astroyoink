b2d30eba48fb8b9f8f1c968df4adad20cf1ae608
Cosmo Ray <Cosmo@cosmo-rays-imac.gateway.sonic.net>
2015-01-04 19:05:21 -0800

    UNABLE TO UNFLEX D:

---
+++ AstroYoink/Assets/Scripts/Flex.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flex : MonoBehaviour {
+
+	public float SpringIntensity = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			gameObject.GetComponent<SpringJoint>().spring =  SpringIntensity;
+			Debug.Log("Flex deez nuts");
+		
+	}
+		else gameObject.GetComponent<SpringJoint>().spring =  0f;
+	
+	}
+}
---
+++ AstroYoink/Assets/Scripts/UnFlex.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnFlex : MonoBehaviour {
+
+	public float SpringIntensity = 100f;
+	JointLimits limits;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.T))
+		{
+			limits = GetComponent<HingeJoint>().limits;
+			limits.min = 0f;
+			limits.max = 0f;
+
+			/*
+			JointLimits limits = hingeJoint.limits;
+			limits.min = 0f;
+			limits.max = 0f;
+			*/
+			Debug.Log("Unflex");
+		
+	}
+		//else 
+	
+	}
+}
